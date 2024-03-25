501972bf947a3decd96c66512e7894588ee2082f
Darkseid-maker <80199377+Darkseid-maker@users.noreply.github.com>
2024-03-25 12:47:45 +0100

    Forth commit

---
+++ Model/Suite_Simple.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Suite_Simple extends Chambre {
+
+    /**
+     * Default constructor
+     */
+    public Suite_Simple() {
+    }
+
+    /**
+     * 
+     */
+    public boolean fenetre;
+
+    /**
+     * 
+     */
+    public int litSimple;
+
+}
