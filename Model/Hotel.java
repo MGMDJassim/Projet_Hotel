87a361ca82bb4dcc4eeca722979cdd8d72333a3d
Darkseid-maker <80199377+Darkseid-maker@users.noreply.github.com>
2024-03-26 23:17:01 +0100

    V1

--- Model/Hotel.java
+++ Model/Hotel.java
@@ -41,7 +41,68 @@ public class Hotel {
         this.listClient.remove(client);
     }
 
+    public void addProduit(Produit produit){
+        this.listProdiot.add(produit);
+    }
 
+    public void removeProduit(Produit produit){
+        this.listProdiot.remove(produit);
+    }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setListechambre(Set<Chambre> listechambre) {
+        this.listechambre = listechambre;
+    }
+
+    public Set<Chambre> getListechambre() {
+        return listechambre;
+    }
+
+    public void setListClient(Set<Client> listClient) {
+        this.listClient = listClient;
+    }
+
+    public Set<Client> getListClient() {
+        return listClient;
+    }
+
+    public void setListProdiot(Set<Produit> listProdiot) {
+        this.listProdiot = listProdiot;
+    }
+
+    public Set<Produit> getListProdiot() {
+        return listProdiot;
+    }
 
 }
