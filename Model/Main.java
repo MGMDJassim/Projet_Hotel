6f862596586600060f637bb43d6559c3a40f4c4d
yummy16 <99326041+yummy16@users.noreply.github.com>
2024-04-05 16:17:26 +0200

    Update Main.java

--- Model/Main.java
+++ Model/Main.java
@@ -1,7 +1,7 @@
 import java.io.File;  
 import java.io.IOException; 
 
-public class CreateFile {
+public class Main {
   public static void main(String[] args) {
     try {
       File myObj = new File("projet.txt");
