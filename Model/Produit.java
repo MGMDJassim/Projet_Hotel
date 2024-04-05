20b0d68485edb553d8d2a6e8d438ad1357f08a72
yummy16 <99326041+yummy16@users.noreply.github.com>
2024-04-05 15:58:02 +0200

    Update Produit.java

--- Model/Produit.java
+++ Model/Produit.java
@@ -1,5 +1,4 @@
 
-//import java.io.*;
 import java.util.*;
 
 public class Produit {
@@ -9,8 +8,46 @@ public class Produit {
     public Hotel hotel;
     public Vector<Consommation> consommation;
     
-    public Produit() {
+    // Constructeur avec paramètres
+    public Produit(String nom, int prix, Hotel hotel, Vector<Consommation> consommation) {
+        this.nom = nom;
+        this.prix = prix;
+        this.hotel = hotel;
+        this.consommation = consommation;
+    }
+    
+    // Getters et Setters
+    public String getNom() {
+        return nom;
     }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public Vector<Consommation> getConsommation() {
+        return consommation;
+    }
+
+    public void setConsommation(Vector<Consommation> consommation) {
+        this.consommation = consommation;
+    }
+    // methode
+    }
 }
