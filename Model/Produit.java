0f9455cc6fc5701874c4e2af07845abdcf77e70d
yummy16 <99326041+yummy16@users.noreply.github.com>
2024-05-03 15:04:42 +0200

    Update Produit.java

--- Model/Produit.java
+++ Model/Produit.java
@@ -6,13 +6,15 @@ public class Produit {
     public String nom;
     public int prix;
     public Hotel hotel;
+    public int quantite;
     public Vector<Consommation> consommation;
     
     // Constructeur avec paramètres
-    public Produit(String nom, int prix, Hotel hotel, Vector<Consommation> consommation) {
+    public Produit(String nom, int prix, Hotel hotel, int quantite, Vector<Consommation> consommation) {
         this.nom = nom;
         this.prix = prix;
         this.hotel = hotel;
+        this.quantite = quantite;
         this.consommation = consommation;
     }
     
@@ -40,13 +42,50 @@ public class Produit {
     public void setHotel(Hotel hotel) {
         this.hotel = hotel;
     }
-
+    public int getQuantite(){
+        return quantite;
+    }
+    public void setQuantite(int quantite){
+        this.quantite = quantite;
+    }
     public Vector<Consommation> getConsommation() {
         return consommation;
     }
-
     public void setConsommation(Vector<Consommation> consommation) {
         this.consommation = consommation;
+    }
+    
+    // methode modifier prix
+    
+    public void modifierPrix(int nouveauPrix){
+        setPrix(nouveauPrix);
+    }
+    
+    // methode ajouter une consommation
+    
+    public void ajouterConsommation(Consommation nouvelleConso){
+        consommation.add(nouvelleConso);
+    }
+
+    //methode pour retirer une consommation
+        
+    public void retirerConsommation(Consommation suppConso){
+        consommation.remove(suppConso);
+    }
+
+    
+    // methode verifier disponibilite produit
+        
+    public boolean verifierDisponibilite(){
+       return quantite > 0;
+    //methode calcul prix
+
+    public void calculPrixTotalConso(){
+    int prixTotal = 0;
+    for(Consommation cons : consommation){
+           prixTotal += cons.getQuantite() * cons.getProduit().getPrix();
+    }
+    return prixTotal;
     }
     // methode affichage
     public void afficherInfo(){
