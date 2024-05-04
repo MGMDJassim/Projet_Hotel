d29077760604e9c82bc9b58de9bd4c81d8bfdad6
yummy16 <99326041+yummy16@users.noreply.github.com>
2024-05-05 00:48:50 +0200

    Update Produit.java (consommation)

--- Model/Produit.java
+++ Model/Produit.java
@@ -50,12 +50,12 @@ public class Produit {
         return quantite;
     }
     public void setQuantite(int quantite){
+        
         if (quantite <=0){
-            System.out.println("la quantitée rentrée doit être supérieure à 0");
-            
+            System.out.println("la quantité rentrée doit être supérieure à 0.");
+    
         } else {
         this.quantite = quantite;
-            
         }
     }
     public Hotel getHotel() {
@@ -74,20 +74,33 @@ public class Produit {
     
     // methode ajouter une nouvelle consommation
     
-    public void ajouterConsommation(Consommation nouvelleConso){
-        consommation.add(nouvelleConso);
+   public void ajouterConsommation(Consommation nouvelleConso) {
+       
+    if (nouvelleConso != null) { // Vérification que la consommation à ajouter n'est pas nulle
+        if (!consommation.contains(nouvelleConso)) { // Vérification que la consommation n'existe pas déjà
+            consommation.add(nouvelleConso);
+            System.out.println("La consommation a été ajoutée avec succès.");
+        } else {
+            System.out.println("Cette consommation existe déjà.");
+        }
+    } else {
+        System.out.println("La consommation à ajouter est nulle.");
     }
+}
 
-    //methode pour retirer une consommation
-        
-    public void retirerConsommation(Consommation suppConso){
-        consommation.remove(suppConso);
-    }
+public void retirerConsommation(Consommation suppConso) {
     
-     public boolean verifierDisponibilite() {
-        return quantiteStock > 0;
+    if (suppConso != null) { // Vérification que la consommation à retirer n'est pas nulle
+        if (consommation.contains(suppConso)) { // Vérification que la consommation à retirer existe dans la liste
+            consommation.remove(suppConso);
+            System.out.println("La consommation a été retirée avec succès.");
+        } else {
+            System.out.println("Cette consommation n'existe pas dans la liste.");
+        }
+    } else {
+        System.out.println("La consommation à retirer est nulle.");
     }
-    
+}
     // methode verifier disponibilite produit
         
     public boolean verifierDisponibilite(){
