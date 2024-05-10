60457a037a727a5ef9499c4f5cd025f24a60cd6c
yummy16 <99326041+yummy16@users.noreply.github.com>
2024-05-10 14:25:34 +0200

    Update Receptionniste.java

--- Model/Receptionniste.java
+++ Model/Receptionniste.java
@@ -5,22 +5,16 @@ import java.util.*;
 public class Receptionniste extends Employe {
     public Vector<Reservation> listResa;
     public Receptionniste(String nomEmploye, int numEmploye, String prenomEmploye, Vector<Reservation> listResa) {
-        super(nomEmploye, numEmploye,prenomEmploye);
+        super(nomEmploye, numEmploye, prenomEmploye);
         this.listResa = listResa;
-
     }
 
-    public void setNomEmploye(String nomEmploye) {
-        this.nomEmploye = nomEmploye;
+    public Vector<Reservation> getListResa() {
+        return listResa;
     }
-    public String getNomEmploye() {
-        return this.nomEmploye;
-    }
-    public void setNumEmploye(int numEmploye) {
-        this.numEmploye = numEmploye;
-    }
-    public int getNumEmploye() {
-        return this.numEmploye;
+
+    public void setListResa(Vector<Reservation> listResa) {
+        this.listResa = listResa;
     }
 
 }
