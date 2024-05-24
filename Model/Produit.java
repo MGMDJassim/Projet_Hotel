7f3e75bff6aeaff3e0c32b99086a17e8cf875b08
yummy16 <99326041+yummy16@users.noreply.github.com>
2024-05-24 21:21:42 +0200

    Update Produit.java

--- Model/Produit.java
+++ Model/Produit.java
@@ -16,6 +16,7 @@ public class Produit {
         this.prix = prix;
         this.hotel = hotel;
         this.stock = stock;
+        this.consommation = new Vector<>();
     }
     
     // Getters et Setters
